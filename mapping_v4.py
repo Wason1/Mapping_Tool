24414f03dd449b86571dcd643b6c237516387ac1
Jason Whittle <jason.j.whittle@gmail.com>
2023-07-31 13:27:57 +1000

    version for doing some cool stuff

--- mapping_v4.py
+++ mapping_v4.py
@@ -11,31 +11,36 @@ def load_spreadsheet(application, spreadsheet_number):
         print(f'Spreadsheet {spreadsheet_number} loaded with shape {df.shape}')
         if spreadsheet_number == 1:
             application.spreadsheet1 = df
-            application.update_dropdown(application.dropdown1, df.columns)
+            application.update_dropdown(application.dropdown1, df.columns, application.column_button1)
             application.load_button1.config(bg='blue')
             application.dropdown1.config(state=NORMAL)
             application.load_button2.config(state=NORMAL) # Enable "Load Spreadsheet 2" button right after Spreadsheet 1 is loaded
         elif spreadsheet_number == 2:
             application.spreadsheet2 = df
-            application.update_dropdown(application.dropdown2, df.columns)
+            application.update_dropdown(application.dropdown2, df.columns, application.column_button2)
             application.load_button2.config(bg='blue')
             application.dropdown2.config(state=NORMAL)
             application.match_button.config(state=NORMAL)  # Enable "Match Data" button right after Spreadsheet 2 is loaded
 
-def match_data(application, s1, s2, progressbar):
+def match_data(application, s1, s2, progressbar, threshold=70):
     length = len(s1)
     progressbar["maximum"] = length
+    application.matches = []  # Create a list to store all matches
     for i in tqdm(range(length), desc="Matching..."):
-        highest = process.extractOne(s1[i], s2, scorer=fuzz.token_sort_ratio)
-        application.add_match(highest)
+        matches = process.extract(s1[i], s2, scorer=fuzz.token_sort_ratio, limit=None)
+        good_matches = [match for match in matches if match[1] >= threshold]
+        application.matches.append((s1[i], good_matches))
         progressbar["value"] = i
         progressbar.update()
+    application.next_item_index = 0  # Initialize index for "Next Item" button
+    application.next_item()  # Display the first item
 
 class Application:
     def __init__(self, master):
         self.master = master
         self.spreadsheet1 = None
         self.spreadsheet2 = None
+
         self.column1 = None
         self.column2 = None
 
@@ -57,17 +62,35 @@ class Application:
         self.load_button2 = Button(master, text="Load Spreadsheet 2", command=lambda: load_spreadsheet(self, 2), state=DISABLED)
         self.load_button2.pack(fill='x')
 
+        self.column_button1 = Button(master, text="Select column from Spreadsheet 1", state=DISABLED)
+        self.column_button1.pack(fill='x')
+
+        self.column_button2 = Button(master, text="Select column from Spreadsheet 2", state=DISABLED)
+        self.column_button2.pack(fill='x')
+
         self.match_button = Button(master, text="Match Data", command=self.match_data, state=DISABLED)
         self.match_button.pack(fill='x')
 
+        self.next_button = Button(master, text="Next Item", command=self.next_item, state=DISABLED)
+        self.next_button.pack(fill='x')
+
         self.save_button = Button(master, text="Save Matches", command=self.save_matches, state=DISABLED)
         self.save_button.pack(fill='x')
+
+        self.close_button = Button(master, text="Close", command=self.close_app)
+        self.close_button.pack(fill='x')
 
         self.progressbar = Progressbar(master, length=500)
         self.progressbar.pack(fill='x')
 
         self.listbox = Listbox(master)  # Use a Listbox for displaying matches
         self.listbox.pack(fill='both', expand=True)
+
+        self.matches = []
+        self.next_item_index = 0
+    
+    def close_app(self):
+        self.master.destroy()
 
     def set_column(self, dropdown, value):
         if dropdown == self.dropdown1:
@@ -78,10 +101,20 @@ class Application:
             self.load_button2.config(text=f"Spreadsheet 2: {value}", bg="blue")
         self.check_if_match_possible()
 
-    def update_dropdown(self, dropdown, options):
+    def update_dropdown(self, dropdown, options, column_button):
         dropdown['menu'].delete(0, 'end')
         for option in options:
-            dropdown['menu'].add_command(label=option, command=lambda value=option: self.set_column(dropdown, value))
+            dropdown['menu'].add_command(label=option, command=lambda value=option: self.set_column(dropdown, value, column_button))
+        column_button.config(state=NORMAL)
+
+    def set_column(self, dropdown, value, column_button):
+        if dropdown == self.dropdown1:
+            self.column1 = value
+            column_button.config(text=f"Spreadsheet 1: {value}", bg="blue")
+        elif dropdown == self.dropdown2:
+            self.column2 = value
+            column_button.config(text=f"Spreadsheet 2: {value}", bg="blue")
+        self.check_if_match_possible()
 
     def match_data(self):
         if self.spreadsheet1 is not None and self.spreadsheet2 is not None and self.column1 is not None and self.column2 is not None:
@@ -104,6 +137,18 @@ class Application:
                 messagebox.showinfo("Success", "Matches saved successfully.")
         else:
             messagebox.showerror("Error", "No matches to save.")
+    
+    def next_item(self):
+        if self.matches and self.next_item_index < len(self.matches):
+            self.listbox.delete(0, END)  # Clear the listbox
+            item, matches = self.matches[self.next_item_index]
+            self.listbox.insert(END, f"Matches for '{item}':")
+            for match in matches:
+                self.listbox.insert(END, str(match))
+            self.next_item_index += 1  # Prepare for the next click of the "Next Item" button
+            self.next_button.config(state=NORMAL if self.next_item_index < len(self.matches) else DISABLED)
+        else:
+            messagebox.showinfo("Info", "No more items to match.")
 
 root = Tk()
 root.state('zoomed')  # To maximize the window
