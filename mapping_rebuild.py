46a9cff6c67f9aea0047aa24e0eafa85d77e25e2
Jason Whittle <jason.j.whittle@gmail.com>
2023-09-27 22:55:01 +1000

    up

--- mapping_rebuild.py
+++ mapping_rebuild.py
@@ -62,31 +62,36 @@ class Application:
         self.next_item_index = 0
         self.selections = {}
 
-def load_spreadsheet(self, spreadsheet_number):
-    filepath = filedialog.askopenfilename(title=f"Open Spreadsheet {spreadsheet_number}", filetypes=(("Excel files", "*.xlsx"), ("CSV files", "*.csv"), ("All files", "*.*")))
-    if filepath:
-        try:
-            if filepath.endswith('.csv'):
-                df = pd.read_csv(filepath)
-            else:
-                df = pd.read_excel(filepath)
+    def load_spreadsheet(self, spreadsheet_number):
+        filepath = filedialog.askopenfilename(title=f"Open Spreadsheet {spreadsheet_number}", filetypes=(("Excel files", "*.xlsx"), ("CSV files", "*.csv"), ("All files", "*.*")))
+        if filepath:
+            try:
+                if filepath.endswith('.csv'):
+                    df = pd.read_csv(filepath)
+                else:
+                    df = pd.read_excel(filepath)
 
-            if spreadsheet_number == 1:
-                self.spreadsheet1 = df
-                self.load_button1.config(bg="blue")
-                self.update_dropdown(self.dropdown1, df.columns)
-                self.dropdown1.config(state=NORMAL)
+                if spreadsheet_number == 1:
+                    self.spreadsheet1 = df
+                    self.load_button1.config(bg="blue")
 
-                # Activate the 'load spreadsheet 2' button
-                self.load_button2.config(state=NORMAL)  # This line makes the button active
+                    # Activate the 'load spreadsheet 2' button
+                    self.load_button2.config(state=NORMAL)  # This line makes the button active
 
-            elif spreadsheet_number == 2:
-                self.spreadsheet2 = df
-                self.load_button2.config(bg="blue")
-                self.update_dropdown(self.dropdown2, df.columns)
-                self.dropdown2.config(state=NORMAL)
-        except Exception as e:
-            messagebox.showerror("Error", f"An error occurred while loading the file:\n{e}")
+                elif spreadsheet_number == 2:
+                    self.spreadsheet2 = df
+                    self.load_button2.config(bg="blue")
+
+                    #Activate dropdown 2
+                    self.update_dropdown(self.dropdown2, df.columns)
+                    self.dropdown2.config(state=NORMAL)
+
+                    #activate dropdown 1
+                    self.update_dropdown(self.dropdown1, df.columns)
+                    self.dropdown1.config(state=NORMAL)
+
+            except Exception as e:
+                messagebox.showerror("Error", f"An error occurred while loading the file:\n{e}")
 
 
             
